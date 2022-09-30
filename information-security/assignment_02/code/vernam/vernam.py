77930ab3b8269def6ab4960d3604b300ff785958
Stijn Kammer <stijnberend@hotmail.com>
2022-09-30 20:04:53 +0200

    Updated files

--- information-security/assignment_02/code/vernam/vernam.py
+++ information-security/assignment_02/code/vernam/vernam.py
@@ -4,13 +4,13 @@ def read_stdin():
     return sys.stdin.buffer.read().split(b'\xff', 1)
 
 def vernam_encrypt_decrypt(input, key):
-    ciphertext = ""
+    ciphertext = []
     for i in range(len(input)):
-        ciphertext += chr(input[i] ^ key[i % len(key)])
-    return ciphertext
+        ciphertext.append(input[i] ^ key[i % len(key)])
+    return bytes(ciphertext)
 
 def main():
-    sys.stdout.buffer.write(vernam_encrypt_decrypt(*read_stdin()).encode())
+    sys.stdout.buffer.write(vernam_encrypt_decrypt(*read_stdin()))
 
 if __name__ == "__main__":
     main()
