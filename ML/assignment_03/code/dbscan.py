699d14accf3458e28559489293b0895e94291110
ramon <ramon_kits@hotmail.nl>
2022-10-05 19:34:36 +0200

    added silhouette scores

--- ML/assignment_03/code/dbscan.py
+++ ML/assignment_03/code/dbscan.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import silhouette_score
 
 
 def dbscan(data, eps, min_pts):
@@ -18,7 +19,13 @@ def dbscan(data, eps, min_pts):
                 noise.append(i)
             else:
                 expandCluster(data, i, neighbors, clusters, eps, min_pts, visited)
-    return clusters, noise
+    # calculate silhouette score
+    labels = np.zeros(len(data))
+    for i in range(len(clusters)):
+        for j in clusters[i]:
+            labels[j] = i
+    silhouetteScore = silhouette_score(data, labels)
+    return clusters, noise, silhouetteScore
 
 
 def expandCluster(data, i, neighbors, clusters, eps, min_pts, visited):
@@ -50,8 +57,12 @@ def distance(p1, p2):
 
 
 def plotClusters(data, eps, k):
+    """
+    Plot the clusters and noise points.
+    Also returns the silhouette score.
+    """
     # run dbscan
-    clusters, noise = dbscan(data, eps, k)
+    clusters, noise, silhouetteScore = dbscan(data, eps, k)
 
     # create len(clusters) colors for plotting using rainbow color map
     colors = plt.cm.rainbow(np.linspace(0, 1, len(clusters)))
@@ -67,6 +78,8 @@ def plotClusters(data, eps, k):
     plt.title('DBSCAN Clustering with eps = ' + str(eps) + ' and k = ' + str(k))
     # save plot to file
     plt.savefig('output/dbscan_eps_' + str(eps) + '_k_' + str(k) + '.png')
+    # return silhouette score
+    return silhouetteScore
 
 
 def elbowPlotKNearestNeighbors(data, k, highligted_point):
@@ -93,18 +106,40 @@ def elbowPlotKNearestNeighbors(data, k, highligted_point):
     plt.title('Elbow Plot for K = ' + str(k))
     # save plot to file
     plt.savefig('output/elbow_plot_k_' + str(k) + '.png')
-    
+
+def showSilhouetteScores(scoreObjects):
+    # show table of silhouette scores using plt
+    plt.figure()
+    plt.axis('off')
+    plt.table(cellText=[
+                    [scoreObject['k'], scoreObject['eps'], scoreObject['score']] for scoreObject in scoreObjects
+                ],
+                colLabels=['k', 'eps', 'silhouette score'],
+                loc='center')
+    plt.savefig('output/silhouette_scores.png')
+
 
 # import data_clustering.csv
 data = pd.read_csv('data_clustering.csv', header=None).values.tolist()
 
+# create dictionary to store silhouette scores
+silhouetteScores = [{'k': 0, 'eps': 0, 'score': 0} for _ in range(3)]
 # create elbow plots for k = 3, 4, 5 to find best eps per k
 # the best eps value is the point where the curvature of the plot is the most pronounced
 elbowPlotKNearestNeighbors(data, 3, 172)
-plotClusters(data, 0.0493, 3)
+silhouetteScores[0]['k'] = 3
+silhouetteScores[0]['eps'] = 0.0493
+silhouetteScores[0]['score'] = plotClusters(data, 0.0493, 3)
 elbowPlotKNearestNeighbors(data, 4, 184)
-plotClusters(data, 0.071, 4)
+silhouetteScores[1]['k'] = 4
+silhouetteScores[1]['eps'] = 0.071
+silhouetteScores[1]['score'] = plotClusters(data, 0.071, 4)
 elbowPlotKNearestNeighbors(data, 5, 180)
-plotClusters(data, 0.0748, 5)
+silhouetteScores[2]['k'] = 5
+silhouetteScores[2]['eps'] = 0.0748
+silhouetteScores[2]['score'] = plotClusters(data, 0.0748, 5)
+
+# show table of silhouette scores
+showSilhouetteScores(silhouetteScores)
 
 plt.show()
