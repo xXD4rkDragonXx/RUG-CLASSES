813127f7eb5ed18156a2a250b9ec635cbf99ee87
ramon <ramon_kits@hotmail.nl>
2022-10-25 14:54:56 +0200

    added in and out for assignment 4 + begin tiger

---
+++ information-security/assignment_04/code/tiger_hash/tiger_hash.py
@@ -0,0 +1,22 @@
+import sys
+
+def read_file(filename):
+    with open(filename, 'rb') as f:
+        return f.read()
+
+def read_stdin():
+    return sys.stdin.buffer.read()
+
+def main():
+    # this makes it possible to read from stdin when no filename is given on execution
+    # execute with: python tiger_hash.py [readfrom]
+    if len(sys.argv) > 1:
+        data = read_file(sys.argv[1])
+    else:
+        data = read_stdin()
+
+    # TODO give output
+    sys.stdout.buffer.write(data)
+
+if __name__ == "__main__":
+    main()
